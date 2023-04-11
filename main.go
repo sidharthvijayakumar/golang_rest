89663f6c442d31205b859c2cf0733a3e990f13b9
Anirudh M Pai <anirudhmpai@users.noreply.github.com>
2023-04-11 17:26:25 +0530

    Update main.go

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ import (
 func main() {
 	r := router.Router()
 	fmt.Println("Starting server on the port 8080...")
-	log.Fatal(http.ListenAndServe("0.0.0.0:8080", r))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
