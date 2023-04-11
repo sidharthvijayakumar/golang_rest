7cf8f5480d46a4dda65727915068f401b2ce3a79
anirudhmpai <anirudhmpai@gmail.com>
2023-04-11 20:33:34 +0530

    update with env

--- main.go
+++ main.go
@@ -4,12 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/anirudhmpai/router"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	r := router.Router()
-	fmt.Println("Starting server on the port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	fmt.Println("Starting server on the port " + os.Getenv("PORT") + "...")
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), r))
 }
